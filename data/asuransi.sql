1e638b133f3397a69f322fcccb809901fa16373b
frankiehuangg <frankie.huangg@gmail.com>
2023-04-05 21:08:44 +0700

    [NEW] queries for asuransi, kelas, and laboratorium

---
+++ data/asuransi.sql
@@ -0,0 +1,16 @@
+create table asuransi (
+	nama_asuransi VARCHAR(50),
+	no_telepon VARCHAR(50),
+	nama_kontak VARCHAR(50),
+	PRIMARY KEY (nama_asuransi)
+);
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Rowe-Armstrong', '118-649-9483', 'Carine Guerrero');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Herman, Romaguera and Simonis', '515-809-2010', 'Correna Twelvetrees');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Turcotte Group', '515-904-4046', 'Emelita McTeggart');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Kuvalis-Erdman', '802-816-1474', 'Pebrook Fice');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Heaney, Hayes and Durgan', '879-797-1610', 'Francesco Labes');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('McDermott, Bashirian and Witting', '942-816-6650', 'Guilbert Ownsworth');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Berge and Sons', '949-914-0704', 'Maren Danaher');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Schoen, Huel and Emard', '183-721-6948', 'Kenn Nolder');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Stanton LLC', '515-414-0363', 'Bernardine Beert');
+insert into asuransi (nama_asuransi, no_telepon, nama_kontak) values ('Batz Group', '326-438-7480', 'Consuelo Greenwood');
---
+++ data/laboratorium.sql
@@ -0,0 +1,16 @@
+create table asuransi (
+	id_lab INT,
+	nama_lab VARCHAR(50),
+	nama_kepala VARCHAR(50),
+	PRIMARY KEY (id_lab)
+);
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (1, 'Bacteriology', 'Elizabet Hammarberg');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (2, 'Virology', 'Inglebert Heino');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (3, 'Mycology', 'Andrea Hirsthouse');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (4, 'Histopathology', 'Alejoa Tweedlie');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (5, 'Immunology', 'Oby Chattell');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (6, 'Cytology', 'Sydel Dils');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (7, 'Parasitology', 'Ara Liddon');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (8, 'Biochemical Analysis', 'Jess Dominguez');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (9, 'Haematology', 'Benoit Gait');
+insert into asuransi (id_lab, nama_lab, nama_kepala) values (10, 'Hormonal Analysis', 'Ana Dobbinson');
