46af5f8c4922ad7d7f243cac583f5db6f43c6c90
frankiehuangg <frankie.huangg@gmail.com>
2023-04-06 16:58:31 +0700

    [FIX] table name

--- data/laboratorium.sql
+++ data/laboratorium.sql
@@ -1,16 +1,16 @@
-create table asuransi (
+create table laboratorium (
 	id_lab INT,
 	nama_lab VARCHAR(50),
 	nama_kepala VARCHAR(50),
 	PRIMARY KEY (id_lab)
 );
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (1, 'Bacteriology', 'Elizabet Hammarberg');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (2, 'Virology', 'Inglebert Heino');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (3, 'Mycology', 'Andrea Hirsthouse');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (4, 'Histopathology', 'Alejoa Tweedlie');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (5, 'Immunology', 'Oby Chattell');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (6, 'Cytology', 'Sydel Dils');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (7, 'Parasitology', 'Ara Liddon');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (8, 'Biochemical Analysis', 'Jess Dominguez');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (9, 'Haematology', 'Benoit Gait');
-insert into asuransi (id_lab, nama_lab, nama_kepala) values (10, 'Hormonal Analysis', 'Ana Dobbinson');
+insert into laboratorium (id_lab, nama_lab, nama_kepala) values (1, 'Bacteriology', 'Elizabet Hammarberg');
+insert into laboratorium (id_lab, nama_lab, nama_kepala) values (2, 'Virology', 'Inglebert Heino');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (3, 'Mycology', 'Andrea Hirsthouse');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (4, 'Histopathology', 'Alejoa Tweedlie');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (5, 'Immunology', 'Oby Chattell');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (6, 'Cytology', 'Sydel Dils');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (7, 'Parasitology', 'Ara Liddon');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (8, 'Biochemical Analysis', 'Jess Dominguez');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (9, 'Haematology', 'Benoit Gait');
+insert into laboratorium (id_lab, na, nama_lab, nama_kepala) values (10, 'Hormonal Analysis', 'Ana Dobbinson');
