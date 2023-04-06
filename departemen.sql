fe13d12def44f028b18bcf2d23fb0be85be8d6da
natthankrish <92136335+natthankrish@users.noreply.github.com>
2023-04-06 13:54:22 +0700

    feat: tenaga medis

--- departemen.sql
+++ departemen.sql
@@ -1,16 +1,16 @@
-CREATE TABLE Departemen (
+CREATE TABLE departemen (
     id_dept char(5),
     nama_dept varchar(50) NOT NULL,
     aset decimal(13,2),
     PRIMARY KEY (id_dept)
 );
 
-INSERT INTO Departemen
+INSERT INTO departemen
     (id_dept, nama_dept, aset)
 VALUES 
     ('DPT01', 'Bedah', 100000), 
     ('DPT02', 'Urologi', 200000),
-    ('DPT03', 'Dematologi', 300000),
+    ('DPT03', 'Dermatologi', 300000),
     ('DPT04', 'Kesehatan Anak', 400000),
     ('DPT05', 'Kesehatan Jiwa', 500000),
     ('DPT06', 'Gigi dan Mulut', 600000),
