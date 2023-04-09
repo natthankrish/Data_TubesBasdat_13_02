6ea0a1fcd352a4223548b215c60d49771a7ba92f
naufal <naufalfirdaus21@gmail.com>
2023-04-09 16:41:08 +0700

    121-150 progress (blm jd)

--- data/rekammedis.sql
+++ data/rekammedis.sql
@@ -1,8 +1,8 @@
-CREATE TABLE rekam_medis {
+CREATE TABLE rekam_medis (
     id_pasien       int,
     id_rekam_medis  int,
     tanggal_masuk   DATE NOT NULL,
     tanggal_keluar  DATE NOT NULL,
     PRIMARY KEY (id_pasien, id_rekam_medis),
     FOREIGN KEY (id_pasien) REFERENCES pasien,
-}
+)
